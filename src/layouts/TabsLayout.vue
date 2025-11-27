f688dbbc727fd84b5fd5b805aceaf0b6faa64e37
雨菡 <13220755219@163.com>
2025-11-27 17:06:10 +0800

    页面布局TabsLayout

--- src/layouts/TabsLayout.vue
+++ src/layouts/TabsLayout.vue
@@ -1,11 +1,94 @@
 <template>
-  主要内容
+  <el-main>
+    <el-tabs v-model="currentTab.name" class="demo-tabs" closable @tab-remove="remove" @tab-click="handleClick" type="card">
+      <el-tab-pane
+        
+        v-for="item in tabs"
+        :key="item.url"
+        :name="item.name"
+        :label="item.name"
+      >
+      <template #label>
+        <span class="custom-tabs-label">
+          <el-icon>
+            <component :is="item.icon"></component>
+          </el-icon>
+          <span>&nbsp;{{ item.name }}</span>
+        </span>
+      </template>
+      </el-tab-pane>
+    </el-tabs>
+    <RouterView v-slot="{Component}">
+      <KeepAlive>
+        <component :is="Component" :key="$route.name" v-if="$route.meta.keepAlive"></component>
+      </KeepAlive>
+      <component :is="Component" :key="$route.name" v-if="!$route.meta.keepAlive"></component>
+    </RouterView>
+  </el-main>
 </template>
 
 <script lang="ts" setup>
+import { useTabsStore } from '@/store/tabs'
+import { storeToRefs } from 'pinia'
+import { useRouter, useRoute } from 'vue-router'
+import { useLoginStore } from '@/store/login'
+import type { TabsPaneContext } from 'element-plus'
+
+const router = useRouter()
+const route = useRoute()
+const tabsStore = useTabsStore()
+const { tabs, currentTab } = storeToRefs(tabsStore)
+const { setCurrentTab, removeTab, addTabs } = tabsStore
+const loginStore = useLoginStore()
+const { menu } = storeToRefs(loginStore)
+
+// 点击tab
+const handleClick = ( pane:TabsPaneContext ) => {
+  // 获取点击标签的名字
+  const clickTab = pane.props.name
+  // 在tabs数组里找到对应的对象
+  const tab = tabs.value.find( item => item.name == clickTab)
+  if(tab){
+    router.push(tab.url)
+    setCurrentTab(tab.name,tab.url)
+  }
+}
+// 关闭tab
+const remove = (name:string) => {
+  removeTab(name)
+  router.push(currentTab.value.url)
+}
+ const findObjectByUrl = (arr:any[], url:string) => {
+    for(const item of arr){
+      if(item.url === url){
+        return item
+      }
+      if(item.children){
+        const found:any = findObjectByUrl(item.children,url)
+        if(found){
+          return found
+        }
+      }
+    }
+    return null
+ }
+const {name, url, icon} = findObjectByUrl(menu.value, route.path)
+addTabs(name,url,icon)
+setCurrentTab(name,url)
 
 </script>
 
-<style scoped>
+<style scoped lang="less">
+.demo-tabs{
+  :deep(.is-active){
+    background-color: rgb(95, 166, 252) !important;
+    color: white !important
+  }
+  .custom-tabs-label{
+    display:flex;
+    align-items:center;
+    justify-content: center;
+  }
+}
 
 </style>
--- src/store/tabs.ts
+++ src/store/tabs.ts
@@ -1,32 +1,63 @@
-// 侧边栏点击后，不仅要跳转路由，还要在顶部添加页签
 import { defineStore } from 'pinia'
-import type { MenuItem } from '@/type/menuTy'
 import { ref } from 'vue'
+import type { MenuItem } from '@/type/menuTy'
 
- export const useTabsStore = defineStore('tabs',() => {
-    // 默认存在一个首页标签
-    const tabs = ref<MenuItem[]>([
-        {name:'数据看板', url:'/dashboard', icon:'DataLine'}
-    ])
-    // 当前激活的标签
-    const currentTab = ref<{name:string,url:string}>({
-        name:'数据看板',
-        url:'/dashboard'
-    })
-    // 添加标签的动作
+export const useTabsStore = defineStore('tabs', () => {
+    // state
+    // 1.标签列表(那一行打开的所有标签)
+    const tabs = ref<MenuItem[]>([])
+    // 2.高亮的标签
+    const currentTab = ref<{name:string, url:string}>(
+        { name:'',url:''}
+    )
+    // action
+    // 1.添加标签
     const addTabs = (name:string,url:string,icon:string) => {
-        if(!tabs.value.some(item => item.url == url)){
+        if(!tabs.value.some( (item) => item.url === url)){
             tabs.value.push({name,url,icon})
         }
     }
-    // 设置当前激活标签的动作
+    // 2.高亮当前标签
     const setCurrentTab = (name:string,url:string) => {
         currentTab.value = { name, url }
     }
-    return { 
+    // 3.关闭标签
+    const removeTab = (name:string) => {
+        // 找到要关闭标签的索引
+        const index = tabs.value.findIndex( (item) => item.name === name )
+        // 关闭标签(把要关闭标签之外的标签留下)
+        tabs.value = tabs.value.filter( (item) => item.name !== name )
+        // 如果关闭的是当前高亮的标签
+        if(name === currentTab.value.name){
+            // 如果还有剩余标签
+            if(tabs.value.length > 0){
+                let nextTab :MenuItem | undefined
+                // 如果关闭的是第一个标签，将新的第一个标签高亮
+                if(index === 0){
+                    nextTab = tabs.value[0]
+                }
+                // 如果关闭的不是第一个标签，将前一个标签高亮
+                else{
+                    nextTab = tabs.value[index-1]
+                }
+                if(nextTab){
+                    currentTab.value = {
+                        name:nextTab.name,
+                        url:nextTab.url
+                    }
+                }
+            } 
+            // 没有剩余标签就清空高亮标签
+            else{
+                currentTab.value = { name:'', url:''}
+            }
+        } 
+    }
+    return {
         tabs,
         currentTab,
         addTabs,
-        setCurrentTab
-     }
+        setCurrentTab,
+        removeTab
+    }
 })
